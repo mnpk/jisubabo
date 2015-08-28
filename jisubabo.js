f160906e7e5cc3daacd5c3c93ba2e1ae9db4a6c8
mnpk <mwpark@castis.com>
2015-08-28 12:56:19 +0900

    fix port

--- jisubabo.js
+++ jisubabo.js
@@ -14,7 +14,10 @@ app.get('/baboya', function(req, res) {
   res.send('지수 바보');
 });
 
-var server = app.listen(3000, function () {
+
+var port = process.env.PORT || 8080;
+
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
